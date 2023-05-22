4cdcba78620395c9d0561b8f064397fb8723aee9
Lucas Rodrigues Cunha <123827882+LucasRodriguesCunha@users.noreply.github.com>
2023-05-22 10:50:44 -0300

    prog0316.c

---
+++ prog0316.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+
+    int num1, num2, res = 0;
+    char op;
+
+    printf("Escreva uma expressao: ");
+    scanf("%d %c %d", &num1, &op, &num2);
+
+    switch (op)
+    {
+        case '+': res = num1 + num2;
+        break;
+        case '-': res = num1 - num2;
+        break;
+        case '*': 
+        case 'x':
+        case 'X': res = num1 * num2;
+        break;
+        case '/':
+        case '\\':
+        case ':': res = num1 / num2;
+        break;
+    }
+    
+    printf("%d %c %d = %d\n", num1, op, num2, res);
+    
+    return 0;
+
+}
